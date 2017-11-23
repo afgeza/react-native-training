017078d43f573fc64a96b685b85eea62c9d74a99
Afgeshza <afgeshza@gmail.com>
2017-11-23 09:47:07 +0700

    change class to function

--- src/contact/ContactItem.js
+++ src/contact/ContactItem.js
@@ -13,24 +13,23 @@ type Props = {
   contact: Contact
 };
 
-export default class ContactItem extends Component<Props> {
-  render() {
-    let { name, phoneNumber } = this.props.contact;
+export default function ContactItem(props: Props) {
+  let { name, phoneNumber } = props.contact;
 
-    let onPress = () =>
-      Alert.alert("Contact pressed", `${name}: ${phoneNumber}`);
-
-    return (
-      <Touchable style={Styles.contact} onPress={onPress}>
-        <View style={Styles.contactPhoto}>
-          <Text style={Styles.contactPhotoAlias}>{name[0]}</Text>
-        </View>
-
-        <View style={Styles.contactDetails}>
-          <Text style={Styles.contactName}>{name}</Text>
-          <Text>{phoneNumber}</Text>
-        </View>
-      </Touchable>
-    );
+  function onPress() {
+    Alert.alert("Contact pressed", `${name}: ${phoneNumber}`);
   }
+
+  return (
+    <Touchable style={Styles.contact} onPress={onPress}>
+      <View style={Styles.contactPhoto}>
+        <Text style={Styles.contactPhotoAlias}>{name[0]}</Text>
+      </View>
+
+      <View style={Styles.contactDetails}>
+        <Text style={Styles.contactName}>{name}</Text>
+        <Text>{phoneNumber}</Text>
+      </View>
+    </Touchable>
+  );
 }
